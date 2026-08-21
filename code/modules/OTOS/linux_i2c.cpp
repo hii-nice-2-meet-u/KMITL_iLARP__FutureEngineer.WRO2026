00965b6c26d0f6bbb3fc53e00df7acf644b94170
JUKKRUW <paramaet.oun@gmail.com>
2026-08-21 15:06:00 +0700

    Add LinuxI2C class for I2C bus communication

---
+++ code/modules/OTOS/linux_i2c.cpp
@@ -0,0 +1,74 @@
+#include "linux_i2c.hpp"
+#include <cstddef>
+
+bool LinuxI2C::openBus(std::uint8_t bus_num) {
+
+	fd_ = open(("/dev/i2c-" + std::to_string(bus_num)).c_str(), O_RDWR);
+
+	if (fd_ < 0) {
+		return false;
+	}
+
+	return true;
+}
+
+sfTkError_t LinuxI2C::ping() {
+	if (fd_ < 0) {
+		return ksfTkErrBusNotInit;
+	}
+
+	if (ioctl(fd_, I2C_SLAVE, address()) < 0) {
+		return ksfTkErrBusNoResponse;
+	}
+
+	return ksfTkErrOk;
+}
+
+sfTkError_t LinuxI2C::writeRegister(
+	uint8_t *devReg, size_t regLength, const uint8_t *data, size_t length) {
+
+	if (fd_ < 0) return ksfTkErrBusNotInit;
+
+	if (ioctl(fd_, I2C_SLAVE, address()) < 0) return ksfTkErrBusNoResponse;
+
+	std::vector<uint8_t> buffer;
+
+	if (devReg != nullptr && regLength > 0) {
+		buffer.insert(buffer.end(), devReg, devReg + regLength);
+	}
+
+	if (data != nullptr && length > 0) {
+		buffer.insert(buffer.end(), data, data + length);
+	}
+
+	const ssize_t written = write(fd_, buffer.data(), buffer.size());
+
+	if (written != static_cast<ssize_t>(buffer.size())) {
+		return ksfTkErrFail;
+	}
+
+	return ksfTkErrOk;
+}
+
+sfTkError_t readRegister(
+	uint8_t *devReg, size_t regLength, uint8_t *data, size_t length) {
+
+	if (fd_ < 0) return ksfTkErrBusNotInit;
+
+	if (ioctl(fd_, I2C_SLAVE, address()) < 0) return ksfTkErrBusNoResponse;
+
+	if (devReg != nullptr && regLength > 0) {
+
+		const ssize_t written = write(fd_, devReg, regLength);
+
+		if (written != static_cast<ssize_t>(regLength)) {
+			return ksfTkErrFail;
+		}
+	}
+
+	const ssize_t received = read(fd_, data, length);
+
+	if (received != static_cast<ssize_t>(length)) return ksfTkErrBusUnderRead;
+
+	return ksfTkErrOk;
+}
---
+++ code/modules/OTOS/linux_i2c.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <string>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include <sfTk/sfTkII2C.h>
+
+class LinuxI2C : public sfTkII2C {
+  public:
+	LinuxI2C();
+	~LinuxI2C();
+
+	bool openBus(std::uint8_t bus_num);
+	void closeBus();
+
+	sfTkError_t ping() override;
+
+	sfTkError_t writeRegister(uint8_t *devReg, size_t regLength,
+		const uint8_t *data, size_t length) override;
+
+	sfTkError_t readRegister(uint8_t *devReg, size_t regLength, uint8_t *data,
+		size_t length) override;
+
+  private:
+	int fd_ = -1;
+};
